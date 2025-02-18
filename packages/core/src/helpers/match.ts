678c2edea1a5e74f61c701ff125da0370e561d99
Johnny Carreiro <johnnycarreiro@Johnnys-MacBook-Pro.local>
2025-02-18 14:17:22 -0300

    feat: creates a matcher function to helps act based on Option or Result types

---
+++ packages/core/src/helpers/match.ts
@@ -0,0 +1,45 @@
+import { Result } from "../result";
+import { Option } from "../option";
+
+function match<T, E, R>(
+	matcher: Result<T, E> | Option<T>,
+	cases: {
+		Ok?: (value: T) => R;
+		Err?: (error: E) => R;
+		Some?: (value: T) => R;
+		None?: () => R;
+	},
+): R {
+	if ("isOk" in matcher && matcher.isOk()) {
+		if (!cases.Ok) {
+			throw new Error("Missing case for Ok");
+		}
+		return cases.Ok(matcher.unwrap());
+	}
+
+	if ("isErr" in matcher && matcher.isErr()) {
+		if (!cases.Err) {
+			throw new Error("Missing case for Err");
+		}
+		return cases.Err(matcher.unwrapErr());
+	}
+
+	if ("isSome" in matcher && matcher.isSome()) {
+		if (!cases.Some) {
+			throw new Error("Missing case for Some");
+		}
+		return cases.Some(matcher.unwrap());
+	}
+
+	if ("isNone" in matcher && matcher.isNone()) {
+		if (!cases.None) {
+			throw new Error("Missing case for None");
+		}
+		return cases.None();
+	}
+
+	throw new Error("Invalid matcher or missing case");
+}
+
+// export { match };
+(global as any).match = match;
