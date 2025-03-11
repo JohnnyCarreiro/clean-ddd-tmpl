750f061a9cee2816a031c95670aae7e0861b1d59
Johnny Carreiro <johnnycarreiro@Johnnys-MacBook-Pro.local>
2025-03-11 16:20:46 -0300

    fix: fixes global type inference

--- packages/core/src/helpers/match.ts
+++ packages/core/src/helpers/match.ts
@@ -1,7 +1,21 @@
 import { Result } from "../result";
 import { Option } from "../option";
 
-function match<T, E, R>(
+function match<T, E extends Error, R>(
+	matcher: Result<T, E>,
+	cases: {
+		Ok: (value: T) => R;
+		Err: (error: E) => R;
+	},
+): R;
+function match<T, R>(
+	matcher: Option<T>,
+	cases: {
+		Some: (value: T) => R;
+		None: () => R;
+	},
+): R;
+function match<T, E extends Error, R>(
 	matcher: Result<T, E> | Option<T>,
 	cases: {
 		Ok?: (value: T) => R;
@@ -11,35 +25,26 @@ function match<T, E, R>(
 	},
 ): R {
 	if ("isOk" in matcher && matcher.isOk()) {
-		if (!cases.Ok) {
-			throw new Error("Missing case for Ok");
-		}
+		if (!cases.Ok) throw new Error("Missing case for Ok");
 		return cases.Ok(matcher.unwrap());
 	}
 
 	if ("isErr" in matcher && matcher.isErr()) {
-		if (!cases.Err) {
-			throw new Error("Missing case for Err");
-		}
-		return cases.Err(matcher.unwrapErr());
+		if (!cases.Err) throw new Error("Missing case for Err");
+		return cases.Err(matcher.unwrapErr() as E);
 	}
 
 	if ("isSome" in matcher && matcher.isSome()) {
-		if (!cases.Some) {
-			throw new Error("Missing case for Some");
-		}
+		if (!cases.Some) throw new Error("Missing case for Some");
 		return cases.Some(matcher.unwrap());
 	}
 
 	if ("isNone" in matcher && matcher.isNone()) {
-		if (!cases.None) {
-			throw new Error("Missing case for None");
-		}
+		if (!cases.None) throw new Error("Missing case for None");
 		return cases.None();
 	}
 
 	throw new Error("Invalid matcher or missing case");
 }
 
-// export { match };
 (global as any).match = match;
